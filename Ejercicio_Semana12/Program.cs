73502f4d80b89adece0e84a372d9fe8384805825
Alex <parraalex874@gmail.com>
2025-11-09 20:15:45 -0500

    Bro

---
+++ Ejercicio_Semana12/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Semana12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alexander Fenrnaod Parra De La Cruz");
+        }
+    }
+}
